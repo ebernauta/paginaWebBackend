78735e95b0e2d9ae6f7d136a26d85678659f3a6b
Ebernauta <ebernauta18@gmail.com>
2022-09-13 17:59:18 -0300

    funciones nuevas

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import modelo.mainCalculadora as mainCalculadora
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("main.html")
 
+@app.route("/formulario")
+def formulario():
+    return render_template("formulario.html")
 
+@app.route("/calculadora")
+def calculadoraWeb():
+    return render_template("calculadora.html")
 
-
-
+@app.route("/calcular", methods=["POST", "GET"])
+def calcular():
+    a = request.form["input1"]
+    b = request.form["input2"]
+    #operacion = request.form["operacion"]
+    #filtro para saber que opcion eligió (sumar - restar - multiplicar - dividir) para despues hacer
+    #los calculos 
+    valores = mainCalculadora.calculos.sumar(a,b)
+    return f"La operacion es: {valores}"
 
 
 app.run(debug= True)

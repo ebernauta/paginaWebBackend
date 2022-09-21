243f2888dfb19f46e46b91f6aa9d5afb14226c04
Ebernauta <ebernauta18@gmail.com>
2022-09-20 21:25:05 -0300

    Formulario recibiendo datos

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import re
 from flask import Flask, render_template, request, redirect, url_for
 import modelo.mainCalculadora as mainCalculadora
+import dao.conexion as conexion
+import modelo.mainFormulario as mainFormulario
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +12,21 @@ def main():
 @app.route("/formulario")
 def formulario():
     return render_template("formulario.html")
+
+@app.route("/formularioEnviado", methods=["GET", "POST"])
+def reciboFormulario():
+    identificador = request.form["identificador"]
+    nombre = request.form["nombre"]
+    cantidad = request.form["cantidad"]
+    categorias = request.form["categorias"]
+    ingreso = request.form["ingreso"]
+    perecible = request.form["eleccion"]
+    tipoVenta = request.form["opcion"]
+    
+    f = mainFormulario.formulario(identificador, nombre, cantidad, categorias, ingreso, perecible, tipoVenta)
+    f.agregar()
+    
+    return f"datos recibidos: {identificador}, {nombre}, {cantidad}, {categorias}, {ingreso}, {perecible}, {tipoVenta}"
 
 @app.route("/calculadora")
 def calculadoraWeb():
@@ -21,7 +39,7 @@ def calcular():
     operacion = request.form["operacion"]
     
     if operacion == "sumar":
-        valores = mainCalculadora.calculos.sumar(a, b)
+        valores = mainCalculadora.calculos.sumar(a,b)
         return render_template("calculadora.html", valor=valores)
     elif operacion == "restar":
         valores = mainCalculadora.calculos.restar(a, b)
